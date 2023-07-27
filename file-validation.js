9952309d42f96dfc6603593c3f9c85e1213aee8b
Sourov Pal <85163889+sourovpal@users.noreply.github.com>
2023-07-27 14:22:31 +0530

    Create file-validation.js

---
+++ file-validation.js
@@ -0,0 +1,46 @@
+/*=================================================================================
+                                Dependencies
+=================================================================================== */
+const { checkSchema } = require('express-validator');
+// index.js express app use 
+const fileUpload = require('express-fileupload')
+app.use(fileUpload());
+
+
+
+
+const AdminCreateValidation = checkSchema({
+    avatar: {
+        custom: {
+          options: (value, { req, location, path }) => {
+            var ext = '';
+            var mimetype = '';
+            var mims = ['jpg', 'png', 'gif'];
+            var type = 'image'; // application, image
+            if(!req.files && !req.files['avatar'] && req.files['avatar'].name == ''){
+
+                return Promise.reject('Avatar field is required.');
+
+            }else{
+
+                mimetype = req.files['avatar'].mimetype.split('/').shift().toLowerCase();
+                ext = req.files['avatar'].name.split('.').pop().toLowerCase();
+                
+                if(mimetype !== type){
+                    return Promise.reject(`The post file must be ${type}.`);
+                }
+                
+                if(ext != '' && !mims.includes(ext)){
+                    return Promise.reject('The post image must be a file of type: '+mims.join(', '));
+                }
+            }
+            return true;
+          },
+        },
+      },
+});
+
+
+module.exports = {
+    AdminCreateValidation,
+}
